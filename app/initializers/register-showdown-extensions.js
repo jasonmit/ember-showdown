0366c1f93cee7bb9ec3205d51cd7154bdc8d1d30
Dean Mraz <deanm@runtriz.com>
2017-08-23 21:03:00 -0700

    add example fastboot issue

---
+++ app/initializers/register-showdown-extensions.js
@@ -0,0 +1,14 @@
+export function initialize() {
+  showdown.extension("myExtensionName", function() {
+    return [{
+      type: 'html',
+      regex: '<blockquote>',
+      replace: '<blockquote class="blockquote">'
+    }];
+  });
+}
+
+export default {
+  name: 'register-showdown-extensions',
+  initialize
+};
